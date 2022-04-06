6068de1e38ae649e81e9749798db16d02c8ef75b
Somyajain1909 <202052339@iiitvadodara.ac.in>
2022-04-06 11:52:57 +0530

    new change

---
+++ vidflix/src/components/header/styles/header.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/macro';
+
+export const Background= styled.div``;
+export const Frame= styled.div``;
+export const Conatiner= styled.div``;
+export const Logo= styled.img``;
--- vidflix/src/pages/home.js
+++ vidflix/src/pages/home.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footers';
+
 import { JumbotronContainer } from '../containers/jumbotron';
 
 export default function Home(){
   console.log("HOME Page");
   return (
     <>
+    <HeaderConatiner>
     <JumbotronContainer />
       <FaqsContainer />
     <FooterContainer />
+    </HeaderConatiner>
     </>
   );
 };
